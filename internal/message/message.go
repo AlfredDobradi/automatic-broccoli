c52c235016e48425f1b1093965865bd4f856afd0
Alfred Dobradi <alfreddobradi@gmail.com>
2018-02-20 12:52:25 +0000

    Issue #11 - Add recipient field and Parse function

--- internal/message/message.go
+++ internal/message/message.go
@@ -1,9 +1,39 @@
 package message
 
+import (
+	"regexp"
+)
+
 // Message represents a message document
 type Message struct {
-	Type    string `json:"type"`
-	Time    int64  `json:"time"`
-	User    string `json:"user"`
-	Message string `json:"message"`
+	Type      string `json:"type"`
+	Time      int64  `json:"time"`
+	User      string `json:"user"`
+	Recipient string `json:"recipient"`
+	Message   string `json:"message"`
+}
+
+// Parse gets recipient and message from string
+func Parse(msg string) map[string]string {
+	r, err := regexp.CompilePOSIX(`^@([A-Za-z0-9\-_]+) (.+)`)
+	if err != nil {
+		return map[string]string{
+			"recipient": "",
+			"message":   msg,
+		}
+	}
+
+	matches := r.FindSubmatch([]byte(msg))
+
+	if matches == nil {
+		return map[string]string{
+			"recipient": "",
+			"message":   msg,
+		}
+	}
+
+	return map[string]string{
+		"recipient": string(matches[1]),
+		"message":   string(matches[2]),
+	}
 }
